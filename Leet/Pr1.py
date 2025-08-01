d0a0094a5fb7222685f56ffc6f09caabcdf0417b
Vishnu Vardan <vian.codes@gmail.com>
2025-07-31 22:05:16 -0400

    Create Pr1.py

---
+++ Leet/Pr1.py
@@ -0,0 +1,7 @@
+def twoSum(nums, target):
+    lookup = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in lookup:
+            return [lookup[diff], i]
+        lookup[num] = i
