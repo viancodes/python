2e5eee157922ba0f28125dbacb6f897d49f5f3bf
Vishnu Vardan <vian.codes@gmail.com>
2025-08-02 21:04:08 -0400

    Create isPalindrome.py

---
+++ Leet/isPalindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # For even and odd digit cases
+        return x == reversed_half or x == reversed_half // 10
