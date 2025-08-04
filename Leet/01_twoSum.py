b0c17a2ef1760414e54c2ec9ed4a6af5fe81d176
Vishnu Vardan <vian.codes@gmail.com>
2025-08-04 10:39:07 -0400

    Update and rename I_twoSum.py to 01_twoSum.py

---
+++ Leet/01_twoSum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums, target):
+        lookup = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in lookup:
+                return [lookup[diff], i]
+            lookup[num] = i
--- Leet/I_twoSum.py
+++
@@ -1,7 +0,0 @@
-def twoSum(nums, target):
-    lookup = {}
-    for i, num in enumerate(nums):
-        diff = target - num
-        if diff in lookup:
-            return [lookup[diff], i]
-        lookup[num] = i
