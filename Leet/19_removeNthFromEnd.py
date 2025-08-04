7399fcf7f75a9e3a35a997004b2776056f3adc15
Vishnu Vardan <vian.codes@gmail.com>
2025-08-04 10:58:44 -0400

    Create 19_removeNthFromEnd.py

---
+++ Leet/19_removeNthFromEnd.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0, head)
+        fast = slow = dummy
+
+        # Move fast pointer n+1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+
+        # Move both pointers until fast reaches the end
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        # Skip the target node
+        slow.next = slow.next.next
+
+        return dummy.next
