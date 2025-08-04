36c7938cf5692f790613b7d42dfa2c4f49434a1b
Vishnu Vardan <vian.codes@gmail.com>
2025-08-04 10:41:14 -0400

    Update and rename III_lengthOfLongestSubstring.py to 03_lengthOfLongestSubstring.py

---
+++ Leet/03_lengthOfLongestSubstring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        char_index = {}
+        left = 0
+        max_length = 0
+
+        for right, char in enumerate(s):
+            if char in char_index and char_index[char] >= left:
+                left = char_index[char] + 1
+
+            char_index[char] = right
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
