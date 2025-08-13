8bf0799fdeaca5469a9cb6b6c58fb22e44b6615e
Vishnu Vardan <vian.codes@gmail.com>
2025-08-13 18:25:31 -0400

    Create 56_merge.py

---
+++ Leet/56_merge.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+
+        for start, end in intervals:
+            if not merged or start > merged[-1][1]:
+                merged.append([start, end])
+            else:
+                merged[-1][1] = max(merged[-1][1], end)
+
+        return merged
