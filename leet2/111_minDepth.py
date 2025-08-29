1a82a4929ad8e40c3c2fc95a53b6682984c9e01f
Vishnu Vardan <vian.codes@gmail.com>
2025-08-28 22:53:41 -0400

    Create 111_minDepth.py

---
+++ leet2/111_minDepth.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        q = deque([(root, 1)])  # (node, depth)
+
+        while q:
+            node, depth = q.popleft()
+
+            # If it's a leaf, we've reached the minimum depth
+            if not node.left and not node.right:
+                return depth
+
+            if node.left:
+                q.append((node.left, depth + 1))
+            if node.right:
+                q.append((node.right, depth + 1))
