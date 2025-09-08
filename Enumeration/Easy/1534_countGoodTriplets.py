5288603aefd7920f081e23839b106278367889fc
Vishnu Vardan <vian.codes@gmail.com>
2025-09-07 23:13:34 -0400

    Create 1534_countGoodTriplets.py

---
+++ Enumeration/Easy/1534_countGoodTriplets.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        n = len(arr)
+        ans = 0
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if abs(arr[i] - arr[j]) <= a:  # prune early
+                    for k in range(j + 1, n):
+                        if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                            ans += 1
+        return ans
